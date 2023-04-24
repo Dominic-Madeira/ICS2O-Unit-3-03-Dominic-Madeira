853dfb4192d60e040208ff137aead1a21eb35ef3
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-04-24 17:19:13 +0000

    finished first draft

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 // Copyright (c) 2023 Dominic M. All rights reserved
 //
 // Created by: Dominic M.
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Created on: March 2023
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function myButtonClicked () {
+  // input
+  const radius = parseFloat(document.getElementById('radius').value)
 
-function myButtonClicked() {
-  alert("Calculation Failed!!")
+  // process
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+
+  // output
+  document.getElementById('volume').innerHTML = 'The Volume is: ' + volume.toFixed(2) + ' mm³'
 }
